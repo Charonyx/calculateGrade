5343a841d568061843023f3e26eac3fac92761a2
Charonyx <churairat.onn@gmail.com>
2021-06-28 06:08:09 +0700

    try to insert element by using loop

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
-// alert("test");
+function createInput() {
+    var numSubject = document.getElementById('numSubject').value;
+    for (var i = 0; i < numSubject; i++) {
+        document.getElementById('insertInput').innerHTML = "eiei" + i;
+        // document.getElementsByName('insertInput').
 
-// version mannual
+        console.log(i);
+    }
+}
+
 function calGrade() {
-    // var unitAll = document.getElementsByName('unit').values;
-    // console.log(unitAll);
-    var unit1 = (document.getElementById('unit1').value) * (document.getElementById('grade1').value);
-    var unit2 = (document.getElementById('unit2').value) * (document.getElementById('grade2').value);
-    var unit3 = (document.getElementById('unit3').value) * (document.getElementById('grade3').value);
-    var unit4 = (document.getElementById('unit4').value) * (document.getElementById('grade4').value);
-    var unit5 = (document.getElementById('unit5').value) * (document.getElementById('grade5').value);
-    alert(unit1);
+    // var unit1 = (document.getElementById('unit1').value) * (document.getElementById('grade1').value);
+    // var unit2 = (document.getElementById('unit2').value) * (document.getElementById('grade2').value);
+    // var unit3 = (document.getElementById('unit3').value) * (document.getElementById('grade3').value);
+    // var unit4 = (document.getElementById('unit4').value) * (document.getElementById('grade4').value);
+    // var unit5 = (document.getElementById('unit5').value) * (document.getElementById('grade5').value);
+    // alert(unit1);
 
     var GPAX    = 0;    //เกรดเฉลี่ย
     var GPA     = 0;    //เกรดแต่ละวิชา
@@ -25,11 +30,12 @@ function calGrade() {
             console.log('unit: ' + unit[i].value);
             console.log('unitAll: ' + unitAll);
             console.log('------\n');
-            // console.log(GPA + '\n');
         }
     }
     GPAX = GPA / unitAll; 
     console.log('GPAX: ' + GPAX);
     document.getElementById('result').value = GPAX;
-    document.getElementById('result2').innerHTML = GPAX;
+    // document.getElementById('result2').innerHTML = GPAX;
+
+    //ui    -> navbar div container position color
 }
