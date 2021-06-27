96a767fb648e63a55b024c8ac26372b3154e318b
Charonyx <churairat.onn@gmail.com>
2021-06-27 23:59:09 +0700

    calgradde

--- js/app.js
+++ js/app.js
@@ -5,8 +5,12 @@ function calGrade() {
     var unitAll = document.getElementsByName('unit').values;
     console.log(unitAll);
     var unit1 = (document.getElementById('unit1').value)*(document.getElementById('grade1').value);
+    var unit2 = (document.getElementById('unit2').value)*(document.getElementById('grade2').value);
+    var unit3 = (document.getElementById('unit3').value)*(document.getElementById('grade3').value);
+    var unit4 = (document.getElementById('unit4').value)*(document.getElementById('grade4').value);
+    var unit5 = (document.getElementById('unit5').value)*(document.getElementById('grade5').value);
 
-    var unit = 
+    // var unit = 
 
     alert(unit1);
     
